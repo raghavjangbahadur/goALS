a949d609ad108ad9783f1a3f7e576d64e7cfb5f4
Raghav Jangbahadur <raghavjang1@gmail.com>
2022-06-28 19:41:59 -0700

    Initial Commit

---
+++ goALS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  goALS
+//
+//  Created by Raghav Jangbahadur on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ goALS/goALSApp.swift
@@ -0,0 +1,17 @@
+//
+//  goALSApp.swift
+//  goALS
+//
+//  Created by Raghav Jangbahadur on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct goALSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
