7c867e7e50898a2ffe773c9e87fc5c61cdcc8ac4
Gyanendra Sinha <gyanendra_sinha@apple.com>
2022-07-28 19:31:37 -0700

    Navigation View

--- goALS/LoginView.swift
+++ goALS/LoginView.swift
@@ -11,32 +11,41 @@ struct LoginView: View {
     @ObservedObject var model: LoginModel
     
     var body: some View {
-        VStack {
-            Image("goals_logo_white")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 150)
-                .padding(50)
-            TextField("Email", text: self.$model.email)
-                .foregroundColor(.white)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 20)
-                .autocapitalization(.none)
-                .padding(.bottom, 30)
-            TextField("Password", text: self.$model.password)
-                .foregroundColor(.white)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 20)
-                .autocapitalization(.none)
-                .padding(.bottom, 20)
-            Button("Log in", action: loginAction)
-            Divider()
-            Spacer()
-            
-            /* need to add a button to go to registration view if user does not have an account, and a button to go to the home page view*/
-            
+        NavigationView {
+            VStack {
+                Image("goals_logo_white")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 150)
+                    .padding(50)
+                TextField("Email", text: self.$model.email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 20)
+                    .autocapitalization(.none)
+                    .padding(.bottom, 30)
+                TextField("Password", text: self.$model.password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 20)
+                    .autocapitalization(.none)
+                    .padding(.bottom, 20)
+                Button("Log in", action: loginAction)
+                Divider()
+                Spacer()
+
+                /* need to add a button to go to registration view if user does not have an account, and a button to go to the home page view*/
+
+                NavigationLink("Hidden nav link", isActive: self.$model.loggedIn) {
+                    ContentView()
+                        .navigationTitle("")
+                        .navigationBarBackButtonHidden(true)
+                }
+                .hidden()
+
+            }
+            .background(Color.red)
         }
-        .background(Color.red)
     }
 
     func loginAction() {
