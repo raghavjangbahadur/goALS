17baf470c516e4b0da97ccd0f8af6b750c5e13fa
raghavjangbahadur <raghavjang1@gmail.com>
2022-08-11 20:21:15 -0700

    patientEditView not previewing dont know why

--- goALS/PatientInfoEditView.swift
+++ goALS/PatientInfoEditView.swift
@@ -11,12 +11,66 @@ struct PatientInfoEditView: View {
     
     @ObservedObject var model = PatientInfoModel()
     
+    @State private var newGender: String = ""
+    @State private var newStage: String = ""
+    @State private var newAge: String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        
+        VStack {
+            HStack {
+                Text("Gender")
+                    .foregroundColor(.teal)
+                Spacer()
+                TextField("New Gender", text: self.$newGender)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 10)
+                    .autocapitalization(.none)
+                    .padding(.bottom)
+            }
+            .padding()
+            HStack {
+                Text("Age")
+                    .foregroundColor(.teal)
+                Spacer()
+                TextField("New Age", text: self.$newAge)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 10)
+                    .autocapitalization(.none)
+                    .padding(.bottom)
+            }
+            .padding()
+            HStack {
+                Text("Stage of ALS \n [Link for more info](https://alsnewstoday.com/stages-of-als/)")
+                    .foregroundColor(.teal)
+                Spacer()
+                TextField("New Stage", text: self.$newStage)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 10)
+                    .autocapitalization(.none)
+                    .padding(.bottom)
+            }
+            .padding()
+            Button("Update data", action: updateData)
+                .padding(.bottom)
+        }
     }
     
     init() {
         model.getSingleData()
+    }
+    
+    func updateData() {
+        if newGender != "" {
+            model.updateDataString("patientGender", newGender)
+        }
+        if newAge != "" {
+            model.updateDataInt("patientAge", Int(newAge) ?? 0)
+        }
+        if newStage != "" {
+            model.updateDataString("stage", newStage)
+        }
     }
 }
 
--- goALS/PatientInfoModel.swift
+++ goALS/PatientInfoModel.swift
@@ -22,7 +22,35 @@ class PatientInfoModel: ObservableObject {
     @Published var newAge : Int = 0
     @Published var newStage : String = ""
     
-    func updateData() {
+    func getSingleData() {
+        let db = Firestore.firestore()
+        guard let userID = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let docRef = db.collection("patients").document(userID)
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let docData = document.data()
+                let patientId = docData!["patientId"] as? String ?? ""
+                let docRef = db.collection("patients").document(patientId)
+                docRef.getDocument { (document, error) in
+                    if let document = document, document.exists {
+                        let docData = document.data()
+                        self.patient = PatientInfo(id: patientId, name: docData!["patientName"] as? String ?? "",
+                                                   gender: docData!["patientGender"] as? String ?? "",
+                                                   age: docData!["patientAge"] as? Int ?? 0,
+                                                   stage: docData!["stage"] as? String ?? "")
+                    } else {
+                        print("Document does not exist")
+                    }
+                }
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
+    
+    /* func updateData() {
         
         //need to implement this for the primary caregiver
         
@@ -37,14 +65,17 @@ class PatientInfoModel: ObservableObject {
                 let patientId = docData!["patientId"] as? String ?? ""
                 db.collection("patients").document(patientId).setData(["patientAge" : self.newAge, "patientGender" : self.newGender, "stage" : self.newStage], merge: true) { error in
                     if error == nil {
-                        getSingleData()
+                        self.getSingleData()
                     }
                 }
             } else {
                 print("Document does not exist")
             }
         }
-        func getSingleData() {
+    }
+    */
+        
+    func updateDataString(_ parameter : String, _ newValue : String) {
             let db = Firestore.firestore()
             guard let userID = Auth.auth().currentUser?.uid else {
                 return
@@ -54,45 +85,59 @@ class PatientInfoModel: ObservableObject {
                 if let document = document, document.exists {
                     let docData = document.data()
                     let patientId = docData!["patientId"] as? String ?? ""
-                    let docRef = db.collection("patients").document(patientId)
-                    docRef.getDocument { (document, error) in
-                        if let document = document, document.exists {
-                            let docData = document.data()
-                            self.patient = PatientInfo(id: patientId, name: docData!["patientName"] as? String ?? "",
-                                                       gender: docData!["patientGender"] as? String ?? "",
-                                                       age: docData!["patientAge"] as? Int ?? 0,
-                                                       stage: docData!["stage"] as? String ?? "")
-                        } else {
-                            print("Document does not exist")
+                    db.collection("patients").document(patientId).setData([parameter : newValue], merge: true) { error in
+                        if error == nil {
+                            self.getSingleData()
                         }
                     }
                 } else {
                     print("Document does not exist")
                 }
-                
+        }
+    }
+            
+            
+    func updateDataInt(_ parameter : String, _ newValue : Int) {
+        let db = Firestore.firestore()
+        guard let userID = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let docRef = db.collection("patients").document(userID)
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let docData = document.data()
+                let patientId = docData!["patientId"] as? String ?? ""
+                db.collection("patients").document(patientId).setData([parameter : newValue], merge: true) { error in
+                    if error == nil {
+                        self.getSingleData()
+                    }
+                }
+            } else {
+                print("Document does not exist")
             }
         }
-        func getData() {
-            let db = Firestore.firestore()
-            db.collection("patients").getDocuments { snapshot, error in
-                if error == nil {
-                    if let snapshot = snapshot {
-                        
-                        DispatchQueue.main.async {
-                            self.list = snapshot.documents.map { d in
-                                return PatientInfo(id: d.documentID, name: d["patientName"] as? String ?? "",
-                                                   gender: d["patientGender"] as? String ?? "",
-                                                   age: d["patientAge"] as? Int ?? 0,
-                                                   stage: d["stage"] as? String ?? "")
-                            }
+    }
+            
+    func getData() {
+        let db = Firestore.firestore()
+        db.collection("patients").getDocuments { snapshot, error in
+            if error == nil {
+                if let snapshot = snapshot {
+                    
+                    DispatchQueue.main.async {
+                        self.list = snapshot.documents.map { d in
+                            return PatientInfo(id: d.documentID, name: d["patientName"] as? String ?? "",
+                                               gender: d["patientGender"] as? String ?? "",
+                                               age: d["patientAge"] as? Int ?? 0,
+                                               stage: d["stage"] as? String ?? "")
                         }
                     }
                 }
-                else {
-                    
-                }
+            }
+            else {
                 
             }
+            
         }
     }
 }
