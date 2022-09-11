e11df1b010f80c52c099e85f74de39e4ac92aca2
raghavjangbahadur <raghavjang1@gmail.com>
2022-09-11 13:13:31 -0700

    tribals

--- goALS/tribalsView.swift
+++ goALS/tribalsView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct tribalsView: View {
     var body: some View {
-        Text("tribals")
+        VStack {
+            Image("goals_logo_white")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 100)
+                .padding(.all)
+            NavigationLink(destination: MainMessagesView()) {
+                ZStack {
+                    RoundedRectangle(cornerRadius:15)
+                        .frame(width: 320, height: 45)
+                        .foregroundColor(.gray)
+                    Text("Individual Messaging")
+                        .foregroundColor(.white)
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+            .padding(.bottom, 20)
+            
+            NavigationLink(destination: GroupChatView()) {
+                ZStack {
+                    RoundedRectangle(cornerRadius:15)
+                        .frame(width: 320, height: 45)
+                        .foregroundColor(.gray)
+                    Text("Circle Messaging")
+                        .foregroundColor(.white)
+                }
+            }
+            .padding(.bottom, 20)
+            
+            Divider()
+            Spacer()
+        }
+        .background(Color.red)
     }
 }
 
