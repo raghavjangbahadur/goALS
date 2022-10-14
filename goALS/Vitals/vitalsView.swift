ecbd77efbf723c514ede44c7075156b86930c9d3
raghavjangbahadur <raghavjang1@gmail.com>
2022-10-13 22:49:34 -0700

    bugs

--- goALS/Vitals/vitalsView.swift
+++ goALS/Vitals/vitalsView.swift
@@ -17,105 +17,105 @@ struct vitalsView: View {
     @State var answered = 0
     
     var body: some View {
-        NavigationView{
+        VStack {
             VStack {
-                VStack {
-                    HStack{
-                        Spacer()
-                        Image("goals_logo_white")
-                             .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 100)
-                        Text("> > >")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                            .font(.system(size: 20))
-                        Image("vitals_logo_white")
-                             .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 125)
-                        Spacer()
-                    }
-                    Divider()
-                }
-                .background(Color("DarkGrey"))
-                NavigationLink(destination: PatientInfoView()) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius:15)
-                            .frame(width: 320, height: 45)
-                            .foregroundColor(Color("LightGrey"))
-                            .shadow(color: Color.black.opacity(0.5), radius: 10)
-                        Text("Key details and information")
-                            .foregroundColor(Color("DarkGrey"))
-                            .fontWeight(.bold)
-                    }
-                    .padding(.top, 30)
-                }
-                .padding(.bottom, 30)
-                
-                NavigationLink(destination: TodosView()) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius:15)
-                            .frame(width: 320, height: 45)
-                            .foregroundColor(Color("LightGrey"))
-                            .shadow(color: Color.black.opacity(0.5), radius: 10)
-                        Text("Daily Checklist")
-                            .foregroundColor(Color("DarkGrey"))
-                            .fontWeight(.bold)
-                    }
-                }
-                .padding(.bottom, 30)
-
-                NavigationLink(destination: Calendar()) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius:15)
-                            .frame(width: 320, height: 45)
-                            .foregroundColor(Color("LightGrey"))
-                            .shadow(color: Color.black.opacity(0.5), radius: 10)
-                        Text("Calendar")
-                            .foregroundColor(Color("DarkGrey"))
-                            .fontWeight(.bold)
-                    }
-                }
-                .padding(.bottom, 30)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius:15)
-                        .frame(width: 320, height: 45)
-                        .foregroundColor(Color("LightGrey"))
-                        .shadow(color: Color.black.opacity(0.5), radius: 10)
-                    Text("Learn more about ALS")
-                        .foregroundColor(Color("DarkGrey"))
+                HStack{
+                    Spacer()
+                    Image("goals_logo_white")
+                         .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 100)
+                    Text("> > >")
+                        .foregroundColor(.white)
                         .fontWeight(.bold)
+                        .font(.system(size: 20))
+                    Image("vitals_logo_white")
+                         .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 125)
+                    Spacer()
                 }
-                .padding(.bottom, 30)
-                .onTapGesture(perform: {
-                    showLearn.toggle()
-                })
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius:15)
-                        .frame(width: 320, height: 45)
-                        .foregroundColor(Color("LightGrey"))
-                        .shadow(color: Color.black.opacity(0.5), radius: 10)
-                    Text("Take the ALS Quiz!")
-                        .foregroundColor(Color("DarkGrey"))
-                        .fontWeight(.bold)
-                }
-                .padding(.bottom, 30)
-                .onTapGesture(perform: {
-                    showQuiz.toggle()
-                })
-                Spacer()
+                Divider()
             }
-            .background(Color.white)
-            .sheet(isPresented: $showQuiz, content: {
-                QuizView(correct: $correct, incorrect: $incorrect, answered: $answered)
+            .background(Color("DarkGrey"))
+            NavigationLink(destination: PatientInfoView()) {
+                ZStack {
+                    RoundedRectangle(cornerRadius:15)
+                        .frame(width: 320, height: 45)
+                        .foregroundColor(Color("LightGrey"))
+                        .shadow(color: Color.black.opacity(0.5), radius: 10)
+                    Text("Key details and information")
+                        .foregroundColor(Color("DarkGrey"))
+                        .fontWeight(.bold)
+                }
+                .padding(.top, 30)
+            }
+            .padding(.bottom, 30)
+            
+            NavigationLink(destination: TodosView()) {
+                ZStack {
+                    RoundedRectangle(cornerRadius:15)
+                        .frame(width: 320, height: 45)
+                        .foregroundColor(Color("LightGrey"))
+                        .shadow(color: Color.black.opacity(0.5), radius: 10)
+                    Text("Daily Checklist")
+                        .foregroundColor(Color("DarkGrey"))
+                        .fontWeight(.bold)
+                }
+            }
+            .padding(.bottom, 30)
+
+            NavigationLink(destination: Calendar()) {
+                ZStack {
+                    RoundedRectangle(cornerRadius:15)
+                        .frame(width: 320, height: 45)
+                        .foregroundColor(Color("LightGrey"))
+                        .shadow(color: Color.black.opacity(0.5), radius: 10)
+                    Text("Calendar")
+                        .foregroundColor(Color("DarkGrey"))
+                        .fontWeight(.bold)
+                }
+            }
+            .padding(.bottom, 30)
+            
+            ZStack {
+                RoundedRectangle(cornerRadius:15)
+                    .frame(width: 320, height: 45)
+                    .foregroundColor(Color("LightGrey"))
+                    .shadow(color: Color.black.opacity(0.5), radius: 10)
+                Text("Learn more about ALS")
+                    .foregroundColor(Color("DarkGrey"))
+                    .fontWeight(.bold)
+            }
+            .padding(.bottom, 30)
+            .onTapGesture(perform: {
+                showLearn.toggle()
             })
-            .sheet(isPresented: $showLearn, content: {
-                LearnView()
+            
+            ZStack {
+                RoundedRectangle(cornerRadius:15)
+                    .frame(width: 320, height: 45)
+                    .foregroundColor(Color("LightGrey"))
+                    .shadow(color: Color.black.opacity(0.5), radius: 10)
+                Text("Take the ALS Quiz!")
+                    .foregroundColor(Color("DarkGrey"))
+                    .fontWeight(.bold)
+            }
+            .padding(.bottom, 30)
+            .onTapGesture(perform: {
+                showQuiz.toggle()
             })
+            Spacer()
         }
+        .background(Color.white)
+        .sheet(isPresented: $showQuiz, content: {
+            QuizView(correct: $correct, incorrect: $incorrect, answered: $answered)
+        })
+        .sheet(isPresented: $showLearn, content: {
+            LearnView()
+        })
+        .navigationBarTitle("")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
